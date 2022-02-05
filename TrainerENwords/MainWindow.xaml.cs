611c8d765b1a8f9a5883e1390c1eed1778130f46
vovaborkovskij@gmail.com <vovaborkovskij@gmail.com>
2022-02-05 12:58:58 +0700

    Добавьте файлы проекта.

---
+++ TrainerENwords/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace TrainerENwords
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //string
+        string line;
+
+        //mass
+        string[] wordsEN = new string[10];
+        string[] wordsRU = new string[10];
+
+        //list
+        List<string> RndWordsRU = new List<string>();
+
+        //int
+        int i = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(@"C:\Users\gr692_bvv\source\repos\TrainerEN\TrainerEN\words.txt"))
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("nope");
+            }
+        }
+    }
+}
