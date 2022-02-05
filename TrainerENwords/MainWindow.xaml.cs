7d68e81e161b1dd146347dc49e9dd7985ef6dd5e
vovaborkovskij@gmail.com <vovaborkovskij@gmail.com>
2022-02-05 14:38:31 +0700

    newnew

--- TrainerENwords/MainWindow.xaml.cs
+++ TrainerENwords/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Windows;
+﻿using System.Windows;
 
 namespace TrainerENwords
 {
@@ -9,19 +7,6 @@ namespace TrainerENwords
     /// </summary>
     public partial class MainWindow : Window
     {
-        //string
-        string line;
-
-        //mass
-        string[] wordsEN = new string[10];
-        string[] wordsRU = new string[10];
-
-        //list
-        List<string> RndWordsRU = new List<string>();
-
-        //int
-        int i = 0;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -29,14 +14,6 @@ namespace TrainerENwords
 
         private void ButtonStart_Clicked(object sender, RoutedEventArgs e)
         {
-            //if (File.Exists(@"C:\Users\gr692_bvv\source\repos\TrainerEN\TrainerEN\words.txt"))
-            //{
-
-            //}
-            //else
-            //{
-            //    MessageBox.Show("nope");
-            //}
             var trainerPage = new TrainerPage();
             trainerPage.Show();
             Close();
--- TrainerENwords/TrainerPage.xaml.cs
+++ TrainerENwords/TrainerPage.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace TrainerENwords
 {
@@ -19,9 +11,94 @@ namespace TrainerENwords
     /// </summary>
     public partial class TrainerPage : Window
     {
+        //string
+        string line;
+        string textPath = @"C:\Users\gr692_bvv\source\repos\TrainerENwords\TrainerENwords\words.txt";
+
+        //mass
+        string[] wordsEN = new string[10];
+        string[] wordsRU = new string[10];
+
+        //list
+        List<string> RndWordsRU = new List<string>();
+
+        //int
+        static int i = 0;
         public TrainerPage()
         {
             InitializeComponent();
+
+            //меняем цвет текста TextBox и убираем текст
+            if (NumberTextBox.IsFocused)
+            {
+                NumberTextBox.Foreground = Brushes.Black;
+            }
+
+            //если файл существует
+            if (File.Exists(textPath))
+            {
+                //Если строки кончились, но массивы еще не заполнены, то вызываем функцию снова
+                while (i != 10)
+                {
+                    CreateMassENRU();
+                }
+
+                //Выводим на экран 10 англ. слов
+                ListViewEN.ItemsSource = wordsEN;
+
+                //Рандомим порядок русс. слов
+                while (RndWordsRU.Count != 10)
+                {
+                    var rnd = new Random();
+                    string rndString = wordsRU[rnd.Next(0, 10)];
+
+                    if (!RndWordsRU.Contains(rndString))
+                    {
+                        RndWordsRU.Add(rndString);
+                    }
+                }
+
+                //Выводим на экран 10 русс. слов
+                ListViewRU.ItemsSource = RndWordsRU;
+            }
+            else
+            {
+                MessageBox.Show("nope");
+            }
+        }
+
+        private void CreateMassENRU()
+        {
+            using (StreamReader sr = new StreamReader(textPath, System.Text.Encoding.UTF8))
+            {
+                //Открываем первую строку файла
+                line = sr.ReadLine();
+
+                //Немного рандома
+                Random rnd = new Random();
+
+                //Рандом по первой строке
+                if (rnd.Next(0, 2) != 0 && i == 0)
+                {
+                    wordsEN[0] = line.Split(' ')[0];
+                    wordsRU[0] = line.Split(' ')[1];
+                    i++;
+                }
+
+                //Рандом по остальным строкам файла
+                while (line != null && i < 10)
+                {
+                    if (rnd.Next(0, 2) != 0)
+                    {
+                        wordsEN[i] = line.Split(' ')[0];
+                        wordsRU[i] = line.Split(' ')[1];
+                        i++;
+                    }
+
+                    //переходим на следующую строку
+                    line = sr.ReadLine();
+                }
+            }
         }
     }
 }
